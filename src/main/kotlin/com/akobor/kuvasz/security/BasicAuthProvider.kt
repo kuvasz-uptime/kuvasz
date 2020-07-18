fb4b3662c963cd135e4d500a50bc0449e9926084
akobor <adam.kobor@makery.co>
2020-07-18 22:01:21 +0200

    Fix authentication in native image

--- src/main/kotlin/com/akobor/kuvasz/security/BasicAuthProvider.kt
+++ src/main/kotlin/com/akobor/kuvasz/security/BasicAuthProvider.kt
@@ -1,6 +1,7 @@
 package com.akobor.kuvasz.security
 
-import io.micronaut.context.annotation.Property
+import io.micronaut.context.annotation.ConfigurationProperties
+import io.micronaut.core.annotation.Introspected
 import io.micronaut.http.HttpRequest
 import io.micronaut.security.authentication.AuthenticationException
 import io.micronaut.security.authentication.AuthenticationFailed
@@ -12,23 +13,27 @@ import io.reactivex.BackpressureStrategy
 import io.reactivex.Flowable
 import io.reactivex.FlowableEmitter
 import org.reactivestreams.Publisher
+import javax.inject.Inject
 import javax.inject.Singleton
 
+@ConfigurationProperties("basic-auth-config")
+@Introspected
+class BasicAuthConfig {
+    var user: String? = null
+    var password: String? = null
+}
+
 @Singleton
-class BasicAuthProvider : AuthenticationProvider {
-
-    @field:Property(name = "micronaut.security.basic-auth.user")
-    var basicAuthUser: String = ""
-
-    @field:Property(name = "micronaut.security.basic-auth.password")
-    var basicAuthPassword: String = ""
+class BasicAuthProvider @Inject constructor(private val authConfig: BasicAuthConfig) : AuthenticationProvider {
 
     override fun authenticate(
         httpRequest: HttpRequest<*>?,
         authenticationRequest: AuthenticationRequest<*, *>
     ): Publisher<AuthenticationResponse> {
         return Flowable.create({ emitter: FlowableEmitter<AuthenticationResponse> ->
-            if (authenticationRequest.identity == basicAuthUser && authenticationRequest.secret == basicAuthPassword) {
+            if (authenticationRequest.identity == authConfig.user &&
+                authenticationRequest.secret == authConfig.password
+            ) {
                 emitter.onNext(UserDetails(authenticationRequest.identity as String, ArrayList()))
                 emitter.onComplete()
             } else {
