3997fb89610d9c37b3eeb4b13640645965dce621
akobor <adam.kobor@makery.co>
2020-08-09 21:12:26 +0200

    Refact jOOQ related extension functions

--- src/main/kotlin/com/kuvaszuptime/kuvasz/util/Jooq+.kt
+++ src/main/kotlin/com/kuvaszuptime/kuvasz/util/Jooq+.kt
@@ -2,12 +2,12 @@
 
 package com.kuvaszuptime.kuvasz.util
 
+import arrow.core.toOption
 import com.kuvaszuptime.kuvasz.models.DuplicationError
 import com.kuvaszuptime.kuvasz.models.PersistenceError
 import org.jooq.Configuration
 import org.jooq.DAO
 import org.jooq.TableRecord
-import org.jooq.TransactionalCallable
 import org.jooq.exception.DataAccessException
 import org.postgresql.util.PSQLException
 
@@ -15,17 +15,8 @@ fun <R : TableRecord<R>, P, T> DAO<R, P, T>.transaction(block: () -> Unit) {
     configuration().dsl().transaction { _: Configuration -> block() }
 }
 
-fun <R : TableRecord<R>, P, T, Result> DAO<R, P, T>.transactionResult(block: () -> Result): Result =
-    configuration().dsl().transactionResult(TransactionalCallable {
-        block()
-    })
-
-fun DataAccessException.toPersistenceError(): PersistenceError {
-    val causeMessage = this.getCause(PSQLException::class.java)?.message
-    return if (causeMessage != null) {
-        when {
-            causeMessage.contains("duplicate key") -> DuplicationError()
-            else -> PersistenceError(causeMessage)
-        }
-    } else PersistenceError(message)
-}
+fun DataAccessException.toPersistenceError(): PersistenceError =
+    getCause(PSQLException::class.java)?.message.toOption().fold(
+        { PersistenceError(message) },
+        { if (it.contains("duplicate key")) DuplicationError() else PersistenceError(it) }
+    )
