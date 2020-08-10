8e7b768c6f92c105d6fe2ea735aa7648c5b97ad6
akobor <adam.kobor@makery.co>
2020-08-10 15:24:29 +0200

    Include the actual OpenAPI spec in the repo

--- src/main/kotlin/com/kuvaszuptime/kuvasz/Application.kt
+++ src/main/kotlin/com/kuvaszuptime/kuvasz/Application.kt
@@ -3,15 +3,27 @@ package com.kuvaszuptime.kuvasz
 import io.micronaut.runtime.Micronaut.build
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
+import io.swagger.v3.oas.annotations.info.Contact
 import io.swagger.v3.oas.annotations.info.Info
 import io.swagger.v3.oas.annotations.security.SecurityScheme
 import io.swagger.v3.oas.annotations.security.SecuritySchemes
+import io.swagger.v3.oas.annotations.tags.Tag
 
 @OpenAPIDefinition(
     info = Info(
         title = "kuvasz",
-        version = "latest"
-    )
+        version = "latest",
+        description = "Kuvasz [pronounce as 'koovas'] is an open-source, headless uptime and SSL monitoring service, " +
+                "built in Kotlin on top of the awesome Micronaut framework",
+        contact = Contact(
+            url = "https://github.com/kuvasz-uptime/kuvasz"
+        )
+    ),
+    tags = [
+        Tag(name = "Security operations"),
+        Tag(name = "Management operations"),
+        Tag(name = "Monitor operations")
+    ]
 )
 @SecuritySchemes(
     SecurityScheme(
