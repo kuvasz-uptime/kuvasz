58f5491b25f4d6321142a14814f3b5be274bf823
Michael Pollind <mpollind@gmail.com>
2020-08-29 13:30:58 -0700

    feat: send telegram messages
    task: https://github.com/kuvasz-uptime/kuvasz/issues/12

---
+++ src/main/kotlin/com/kuvaszuptime/kuvasz/models/TelegramWebhookMessage.kt
@@ -0,0 +1,11 @@
+package com.kuvaszuptime.kuvasz.models
+
+import io.micronaut.core.annotation.Introspected
+
+@Suppress("ConstructorParameterNaming")
+@Introspected
+data class TelegramWebhookMessage (
+    val chat_id: String = "@KuvaszBot",
+    val text: String
+){
+}
