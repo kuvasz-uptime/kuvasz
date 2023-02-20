7737af32fb8200a9f8edcb449c336694719df49f
adamkobor <kobor@lovelysystems.com>
2023-02-20 14:03:24 +0100

    speed up DB truncate in tests

--- src/test/kotlin/com/kuvaszuptime/kuvasz/DatabaseBehaviorSpec.kt
+++ src/test/kotlin/com/kuvaszuptime/kuvasz/DatabaseBehaviorSpec.kt
@@ -4,15 +4,25 @@ import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.core.test.TestCase
 import io.kotest.core.test.TestResult
 import jakarta.inject.Inject
-import org.flywaydb.core.Flyway
+import org.jooq.Configuration
 
 abstract class DatabaseBehaviorSpec(body: BehaviorSpec.() -> Unit = {}) : BehaviorSpec(body) {
 
     @Inject
-    lateinit var flyway: Flyway
+    lateinit var jooqConfig: Configuration
 
     override suspend fun afterContainer(testCase: TestCase, result: TestResult) {
-        flyway.clean()
-        flyway.migrate()
+        jooqConfig.dsl().query(
+            """
+            DO ${'$'}${'$'} DECLARE
+                r RECORD;
+            BEGIN
+                FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'kuvasz' 
+                            AND tablename <> 'flyway_schema_history') LOOP
+                    EXECUTE 'TRUNCATE TABLE ' || quote_ident(r.tablename) || ' CASCADE';
+                END LOOP;
+            END ${'$'}${'$'};
+            """.trimIndent()
+        ).execute()
     }
 }
