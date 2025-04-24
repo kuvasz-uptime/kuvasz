4ac1522df70e073134e3d28e6706fc9c1a2969c2
adamkobor <kobor@lovelysystems.com>
2025-04-24 13:20:51 +0200

    enable SSLValidatorTest again

--- src/test/kotlin/com/kuvaszuptime/kuvasz/services/SSLValidatorTest.kt
+++ src/test/kotlin/com/kuvaszuptime/kuvasz/services/SSLValidatorTest.kt
@@ -1,6 +1,5 @@
 package com.kuvaszuptime.kuvasz.services
 
-import io.kotest.core.annotation.Ignored
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.data.forAll
 import io.kotest.data.headers
@@ -11,39 +10,38 @@ import java.net.URL
 
 /**
  * FIXME
+ * The commented lines are broken, because badssl.com has some issues with their certificates for a while now.
  * Since the logic behind wasn't changed since it's broke, we can assume that SSL check still works, but better stay
  * on the safe side and fix this test later
-*/
-@Ignored("badssl.com seems to be broken")
-class SSLValidatorTest : StringSpec(
-    {
-        val validator = SSLValidator()
+ */
+class SSLValidatorTest : StringSpec({
+    val validator = SSLValidator()
 
-        "validate should return the right result" {
-            table(
-                headers("url", "isValid"),
-                row("https://sha256.badssl.com/", true),
-                row("https://sha384.badssl.com/", true),
-                row("https://sha512.badssl.com/", true),
-                row("https://ecc256.badssl.com/", true),
-                row("https://ecc384.badssl.com/", true),
-                row("https://rsa2048.badssl.com/", true),
-                row("https://rsa4096.badssl.com/", true),
-                row("https://rsa8192.badssl.com/", true),
-                row("https://extended-validation.badssl.com/", true),
+    "validate should return the right result" {
+        table(
+            headers("url", "isValid"),
+            row("https://sha256.badssl.com/", true),
+//                row("https://sha384.badssl.com/", true),
+//                row("https://sha512.badssl.com/", true),
+            row("https://ecc256.badssl.com/", true),
+            row("https://ecc384.badssl.com/", true),
+            row("https://rsa2048.badssl.com/", true),
+            row("https://rsa4096.badssl.com/", true),
+//                row("https://rsa8192.badssl.com/", true),
+//                row("https://extended-validation.badssl.com/", true),
 
-                row("https://expired.badssl.com/", false),
-                row("https://wrong.host.badssl.com/", false),
-                row("https://self-signed.badssl.com/", false),
-                row("https://untrusted-root.badssl.com/", false),
-                row("https://no-common-name.badssl.com/", false),
-                row("https://no-subject.badssl.com/", false),
-                row("https://incomplete-chain.badssl.com/", false)
-            ).forAll { url, isValid ->
-                val result = validator.validate(URL(url))
+            row("https://expired.badssl.com/", false),
+            row("https://wrong.host.badssl.com/", false),
+            row("https://self-signed.badssl.com/", false),
+            row("https://untrusted-root.badssl.com/", false),
+            row("https://no-common-name.badssl.com/", false),
+            row("https://no-subject.badssl.com/", false),
+            row("https://incomplete-chain.badssl.com/", false)
+        ).forAll { url, isValid ->
+            val result = validator.validate(URL(url))
 
-                (if (isValid) result.isRight() else result.isLeft()).shouldBeTrue()
-            }
+            (if (isValid) result.isRight() else result.isLeft()).shouldBeTrue()
         }
     }
+}
 )
