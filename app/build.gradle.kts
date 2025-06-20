4036d95b3bb32b42fefc0e2cc6268829b33bb46a
adamkobor <kobor@lovelysystems.com>
2025-06-20 13:38:22 +0200

    update API docs after rebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -168,7 +168,7 @@ jib {
 val updateApiDoc by tasks.registering(type = Copy::class) {
     dependsOn("kaptKotlin")
     from(layout.buildDirectory.file("tmp/kapt3/classes/main/META-INF/swagger/kuvasz-latest.yml"))
-    into("$rootDir/docs/api-doc")
+    into("$rootDir/docs/docs")
 }
 
 val localDbUrl: String by project
