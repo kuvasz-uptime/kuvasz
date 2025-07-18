51a3104249cf09973c6676169cf6173ce532d874
Borsy Máté <52719798+borsym@users.noreply.github.com>
2025-07-18 16:25:38 +0200

    remove comment

--- model/src/main/kotlin/com/kuvaszuptime/kuvasz/models/handlers/DiscordWebhookMessage.kt
+++ model/src/main/kotlin/com/kuvaszuptime/kuvasz/models/handlers/DiscordWebhookMessage.kt
@@ -5,5 +5,5 @@ import io.micronaut.core.annotation.Introspected
 @Introspected
 data class DiscordWebhookMessage(
     val username: String = "KuvaszBot",
-    val content: String // text?
+    val content: String
 )
