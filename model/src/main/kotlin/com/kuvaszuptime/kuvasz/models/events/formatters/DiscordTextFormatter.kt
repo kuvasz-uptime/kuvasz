55c540b48c6c69718dcdb070d44ec64e4d6b117a
Borsy Máté <52719798+borsym@users.noreply.github.com>
2025-07-18 14:17:25 +0200

    Add text formatter for discord

---
+++ model/src/main/kotlin/com/kuvaszuptime/kuvasz/models/events/formatters/DiscordTextFormatter.kt
@@ -0,0 +1,7 @@
+package com.kuvaszuptime.kuvasz.models.events.formatters
+
+object DiscordTextFormatter: RichTextMessageFormatter() {
+    override fun bold(input: String): String = "**$input**"
+
+    override fun italic(input: String): String = "*$input*"
+}
