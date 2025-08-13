771dfca4fc07bd918d33749631e7e3d412179eed
adamkobor <kobor@lovelysystems.com>
2025-08-13 13:47:20 +0200

    try to make response checker test not flaky #8

--- app/src/test/kotlin/io/micronaut/test/kotest/ProjectConfig.kt
+++ app/src/test/kotlin/io/micronaut/test/kotest/ProjectConfig.kt
@@ -4,15 +4,19 @@ import com.kuvaszuptime.kuvasz.testutils.TestDbContainer
 import com.kuvaszuptime.kuvasz.testutils.TestSMTPExtension
 import io.kotest.core.config.AbstractProjectConfig
 import io.micronaut.test.extensions.kotest5.MicronautKotest5Extension
+import io.reactivex.rxjava3.plugins.RxJavaPlugins
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 object ProjectConfig : AbstractProjectConfig() {
     override fun extensions() = listOf(TestSMTPExtension, MicronautKotest5Extension)
 
     override suspend fun beforeProject() {
         TestDbContainer.start()
+        RxJavaPlugins.setIoSchedulerHandler { Schedulers.trampoline() }
     }
 
     override suspend fun afterProject() {
         TestDbContainer.stop()
+        RxJavaPlugins.reset()
     }
 }
