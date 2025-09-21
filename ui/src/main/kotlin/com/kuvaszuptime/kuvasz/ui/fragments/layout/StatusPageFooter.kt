5a3a66d800d8fa46b1b2e0bb89defbde01c90b91
adamkobor <kobor@lovelysystems.com>
2025-09-21 10:26:37 +0200

    ui: add a "powered by" section to the SP footer

--- ui/src/main/kotlin/com/kuvaszuptime/kuvasz/ui/fragments/layout/StatusPageFooter.kt
+++ ui/src/main/kotlin/com/kuvaszuptime/kuvasz/ui/fragments/layout/StatusPageFooter.kt
@@ -13,14 +13,33 @@ internal fun FlowContent.statusPageFooter(statusPageGeneratedAt: OffsetDateTime)
         div {
             classes(CONTAINER_XL)
             div {
-                classes(ROW, TEXT_CENTER, ALIGN_ITEMS_CENTER)
+                classes(ROW, TEXT_CENTER, ALIGN_ITEMS_CENTER, FLEX_ROW_REVERSE)
                 div {
-                    classes(COL_12, COL_LG_AUTO, MT_3, MT_LG_0)
+                    classes(COL_LG_AUTO, MS_LG_AUTO)
                     ul {
                         classes(LIST_INLINE, LIST_INLINE_DOTS, MB_0)
                         footerListItem(
                             label = Messages.statusPageGeneratedAt(statusPageGeneratedAt.toDateTimeStringWithZone())
                         )
+                    }
+                }
+                div {
+                    classes(COL_12, COL_LG_AUTO, MT_3, MT_LG_0)
+                    ul {
+                        classes(LIST_INLINE, LIST_INLINE_DOTS, MB_0)
+                        li {
+                            classes(LIST_INLINE_ITEM)
+                            span {
+                                +"${Messages.poweredBy()} "
+                                a(
+                                    href = "https://kuvasz-uptime.dev",
+                                ) {
+                                    classes(LINK_SECONDARY)
+                                    targetBlank()
+                                    +"Kuvasz Uptime"
+                                }
+                            }
+                        }
                     }
                 }
             }
