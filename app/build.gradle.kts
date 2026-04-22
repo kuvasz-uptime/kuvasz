8a9f239b994af47412e5949abd2b1328fc85523e
adamkobor <kobor@lovelysystems.com>
2026-04-22 19:44:16 +0200

    change the base image to ubi10-minimal from alpine

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -136,7 +136,7 @@ tasks.withType<Detekt>().configureEach {
 
 jib {
     from {
-        image = "eclipse-temurin:25-jre-alpine-3.23"
+        image = "eclipse-temurin:25-jre-ubi10-minimal"
         platforms {
             platform {
                 os = "linux"
