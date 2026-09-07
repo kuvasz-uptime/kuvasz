0ffd2a7756768188913fe07bf52bf592be8da941
adamkobor <kobor@lovelysystems.com>
2026-09-07 18:00:30 +0200

    ui: bump Alpine.js to 3.17.2

--- ui/src/main/kotlin/com/kuvaszuptime/kuvasz/ui/Layout.kt
+++ ui/src/main/kotlin/com/kuvaszuptime/kuvasz/ui/Layout.kt
@@ -83,7 +83,7 @@ internal fun withLayout(
                     }
                     commonScripts(globals.appVersion)
                     script(src = "/public/ext/js/htmx.2.0.10.min.js") {}
-                    script(src = "/public/ext/js/alpine.3.16.3.min.js") {}
+                    script(src = "/public/ext/js/alpine.3.17.2.min.js") {}
                     script(src = "/public/ext/js/masonry.4.2.2.min.js") {}
                 }
             }
